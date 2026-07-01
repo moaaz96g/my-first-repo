42c0dd00c2ecf4d53f1e0c27cf2ddf82f2fd3267
Moaz Gamil Taher <moo04039@gmail.com>
2026-07-01 20:48:18 +0300

    initial commit for creating 168 users

---
+++ create 168users.py
@@ -0,0 +1,35 @@
+#create the session
+
+import boto3
+import csv
+def client():
+    session_obj=boto3.session.Session()
+    client_obj=session_obj.client("iam")
+    return client_obj
+
+def reading_file(txt):
+    with open(txt,"r") as f:
+        file=csv.DictReader(f)
+        user_details=list(file)
+        return user_details
+def main():
+    user_client=client()
+    user_details=reading_file("IAM_168_users_inventory.csv")
+    
+    for user in user_details:
+        username=user["IAM_User_Name"]
+        userpol=user["PolicyARN"]
+        user_client.create_user(UserName=username)
+        user_client.attach_user_policy(
+    UserName=username,
+    PolicyArn=userpol)
+    print ("all users has been created successfully")
+if __name__=="__main__":
+    main() 
+
+
+
+
+
+   
+
